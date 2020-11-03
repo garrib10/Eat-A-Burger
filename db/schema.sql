e5d1a55dce1e2f582c7ab88f98d7c42a4a185385
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-11-02 21:09:11 -0500

    schema.sql and seeds.sql have been updated

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+Use burgers_db; 
+
+CREATE TABLE burgers (
+id int NOT NULL AUTO_INCREMENT, 
+burger_name VARCHAR(255) NOT NULL, 
+devoured BOOLEAN DEFAULT false, 
+createdAT TIMESTAMP NOT NULL, 
+Primary KEY (id)
+); 
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", false),  ("bacon cheeseburger", false), ("turkey burger", false)
