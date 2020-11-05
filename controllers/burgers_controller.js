6b7a4df23e931a13b1691021331da1ca6c9b3b82
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-11-04 19:59:34 -0500

    burgers_controller updated with router.put with burger.update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,8 @@
 const burger = require("../models/burger");
 const router = require("express").Router();
 
-router.get("/", function(req, res){
-    burger.all(function(burgerData) {
+router.get("/", function (req, res) {
+    burger.all(function (burgerData) {
         console.log(burgerData)
 
         const hbsObject = {
@@ -12,13 +12,29 @@ router.get("/", function(req, res){
     })
 })
 
-router.post("/", function(req, res){
-    burger.create(req.body.burger_name, function(res){
+router.post("/", function (req, res) {
+    burger.create(req.body.burger_name, function (res) {
         console.log(res)
         res.direct("/")
     })
 })
 
 // add update one function for router// 
+router.put("/", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update(condition.req.params.id, function (result) {
+        if (result.changedRows === 0) {
+           return res.status(404).end();
+        }
+           res.status(200).end();
+    }
+
+    );
+});
+
+
 
 module.exports = router;
