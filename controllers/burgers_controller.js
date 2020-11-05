96d0ddbadaca209058323a37dd05af0c6aa53748
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-11-04 22:36:33 -0500

    added buttons to index.handlebars and devoured burgers now shows up on page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,15 +10,15 @@ router.get("/", function (req, res) {
             burgers: burgerData
         }
         res.render("index", hbsObject)
-    })
-})
+    });
+});
 //Create a New Burger//
 router.post("/burgers", function (req, res) {
     burger.create(req.body.burger_name, function (res) {
         console.log(res)
         res.redirect("/")
-    })
-})
+    });
+});
 
 // add update one function for router// 
 router.put("/burgers/:id", function (req, res) {
