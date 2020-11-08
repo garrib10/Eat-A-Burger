4101c064a839f8e4c0319e982457b6e12eab0d71
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-11-07 20:57:16 -0500

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-// add the password code  from last time// 
-    password: process.env.MYSQLPASS,
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQLPASS,
+        database: "burgers_db"
+    });
+};
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
